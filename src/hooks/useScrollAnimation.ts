608d0d27fa4d8f1a619fd7442b12170f9803776c
Ahmed Osman <theagencyagents@gmail.com>
2025-09-25 17:38:53 +0000

    its not working ,glitched ? (_for element <TextType>_)

--- src/hooks/useScrollAnimation.ts
+++ src/hooks/useScrollAnimation.ts
@@ -5,24 +5,19 @@ import { useState, useEffect, useRef } from 'react';
 export const useScrollAnimation = (
   options?: IntersectionObserverInit,
   triggerOnce = true
-): [React.RefObject<HTMLDivElement>, boolean, number] => {
+): [React.RefObject<HTMLDivElement>, boolean] => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState(false);
-  const [animationKey, setAnimationKey] = useState(0);
 
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
       if (triggerOnce) {
         if (entry.isIntersecting) {
           setIsVisible(true);
-          setAnimationKey(prev => prev + 1);
           observer.unobserve(entry.target);
         }
       } else {
         setIsVisible(entry.isIntersecting);
-        if(entry.isIntersecting) {
-          setAnimationKey(prev => prev + 1);
-        }
       }
     }, options);
 
@@ -38,5 +33,5 @@ export const useScrollAnimation = (
     };
   }, [options, triggerOnce]);
 
-  return [containerRef, isVisible, animationKey];
+  return [containerRef, isVisible];
 };
